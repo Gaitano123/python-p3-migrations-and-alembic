9e6bab3585a48269e5be8f6e7fd66a0272d12ff6
Gaitano <gaitano.george@student.moringaschool.com>
2023-08-30 22:34:08 +0300

    Completed Assignment

---
+++ app/migrations/versions/e0a96bf25bfe_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e0a96bf25bfe
+Revises: 
+Create Date: 2023-08-30 20:09:40.575375
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e0a96bf25bfe'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
